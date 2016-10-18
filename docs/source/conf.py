3bd63e86830c1f4b785f115529bececa8f60f6a0
hinest <treverhines@gmail.com>
2016-10-18 17:05:28 -0400

    updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,7 @@ todo_include_todos = False
 html_theme = 'alabaster'
 #html_theme = 'classic'
 #html_theme = 'nature'
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
