cad4c83290433530498197d183ee78c63c3954d3
hinest <treverhines@gmail.com>
2016-10-18 16:45:51 -0400

    removed githubpages extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,8 +32,6 @@
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
